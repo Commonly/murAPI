c05c7081c0abd2e2af92a29b28dbc18a41ac22b1
Josh <jdogdoespvp16@gmail.com>
2016-12-20 21:51:01 -0600

    Add files via upload

--- src/main/java/me/jdog/murapi/api/cmd/CMD.java
+++ src/main/java/me/jdog/murapi/api/cmd/CMD.java
@@ -1,6 +1,15 @@
+/*
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package me.jdog.murapi.api.cmd;
 
-import me.jdog.murapi.api.Utilities;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -11,15 +20,13 @@ public abstract class CMD {
     private String name;
 
     /**
-     *
      * @param name The commands name.
      */
     public CMD(String name) {
-       this.name = name;
+        this.name = name;
     }
 
     /**
-     *
      * @return Return the name of the command.
      */
     public String getName() {
@@ -27,7 +34,6 @@ public abstract class CMD {
     }
 
     /**
-     *
      * @param name The commands name.
      */
     public void setName(String name) {
@@ -35,11 +41,10 @@ public abstract class CMD {
     }
 
     /**
-     *
-     * @param sender The sender of the command.
+     * @param sender  The sender of the command.
      * @param command The command itself.
-     * @param label The commands label.
-     * @param args The commands args.
+     * @param label   The commands label.
+     * @param args    The commands args.
      * @return True or false
      */
     public abstract boolean execute(CommandSender sender, Command command, String label, String[] args);
--- src/main/java/me/jdog/murapi/api/cmd/Mur.java
+++ src/main/java/me/jdog/murapi/api/cmd/Mur.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package me.jdog.murapi.api.cmd;
 
 import me.jdog.murapi.MurAPI;
@@ -17,12 +27,12 @@ public class Mur extends CMD {
 
     @Override
     public boolean execute(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length == 0 || args.length == 1) {
+        if (args.length == 0 || args.length == 1) {
             sender.sendMessage(Color.addColor("&c[mur] &amurAPI version: " + Utilities.getInstance().getVersion(MurAPI.getInstance()) + " &dBukkit/Spigot version: " + MurAPI.sv_version));
             return true;
         }
-        if(args[0].equalsIgnoreCase("debug")) {
-            if(args[1].equalsIgnoreCase("true")) {
+        if (args[0].equalsIgnoreCase("debug")) {
+            if (args[1].equalsIgnoreCase("true")) {
                 MurAPI.getInstance().config.set("debug", true);
                 sender.sendMessage(Color.addColor("&cDebug mode: &7TRUE &fYou will now receive debug alerts from murAPI"));
                 return true;
@@ -31,7 +41,7 @@ public class Mur extends CMD {
                 sender.sendMessage(Color.addColor("&cDebug mode: &7FALSE &fYou will no longer receive debug alerts from murAPI"));
                 return true;
             } else if (args[1].equalsIgnoreCase("status")) {
-                if(MurAPI.getInstance().config.getBoolean("debug")) {
+                if (MurAPI.getInstance().config.getBoolean("debug")) {
                     sender.sendMessage(Color.addColor("&cDebug status: &7TRUE &fYou will get console alerts from murAPI"));
                 } else if (!MurAPI.getInstance().config.getBoolean("debug")) {
                     sender.sendMessage(Color.addColor("&cDebug status: &7FALSE &fYou will not get console alerts from murAPI"));
